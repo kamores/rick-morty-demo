4781b9fb83b877575dfb60f865deb530b35303f9
Karina Moreno <kamores@gmail.com>
2023-03-31 12:31:01 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import imageRickMorty from './img/rick-morty2.jpg';
+import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
+
+function App() {
+
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);
+    //console.log(characterApi);
+
+  };
+  //console.log(characters);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters}
+            setCharacters={setCharacters}
+
+          />
+        ) : (
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home">
+            </img>
+            <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
+      </header>
+    </div>
+  )
+}
+
+export default App;
